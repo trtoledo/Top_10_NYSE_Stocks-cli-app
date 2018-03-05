ba3511bc75dc0de7d7990b78299d15bf4e0be865
Tomas Toledo <trtoledo1977@gmail.com>
2018-03-05 23:52:42 +0000

    Done.

--- lib/Top_10_NYSE_Stocks.rb
+++ lib/Top_10_NYSE_Stocks.rb
@@ -1,4 +1,5 @@
 require "Top_10_NYSE_Stocks/version"
+require "Top_10_NYSE_Stocks/cli"
 
 module Top10NYSEStocks
   # Your code goes here...
---
+++ lib/Top_10_NYSE_Stocks/cli.rb
@@ -0,0 +1,5 @@
+class Top10NYSEStocks::CLI
+
+  def call
+    puts "The top 10 stocks on moviment in the NYSE today are: "
+  end
