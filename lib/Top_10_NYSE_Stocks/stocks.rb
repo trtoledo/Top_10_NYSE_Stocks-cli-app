ad20e837a441b512e1301609a3369d995dc6277e
Tomas Toledo <trtoledo1977@gmail.com>
2018-03-18 13:33:05 +0000

    scraping

--- lib/Top_10_NYSE_Stocks/stocks.rb
+++ lib/Top_10_NYSE_Stocks/stocks.rb
@@ -11,8 +11,37 @@ def self.scrape_stocks
   stocks_today = []
 
   doc = Nokogiri::HTML(open("https://www.nyse.com/index"))
-  all_stocks = doc.search(div.d-container.d-flex-fix.d-vbox.d-nowrap.d-justify-start.ds-simpleTable.d-noscroll)
-  
+  all_stocks = doc.search("div.d-container.d-flex-fix.d-vbox.d-nowrap.d-justify-start.ds-simpleTable.d-noscroll").text
+  #all_stocks.search("ds-row d-hbox d-hover d-pointer")
+
+  def scraping_stocks(all_stocks)
+    stock = all_stocks.search("ds-row d-hbox d-hover d-pointer").each_with_index
+    stock.name = stock.serach("ds-cell d-flex5")
+    stock_1 = self.new
+    stock_1.name = "name 1"
+    stock_1.volume = "volume 1"
+    stock_1.last = "last 1"
+    stock_1.change = "change 1"
+    stock_1.url = "url 1"
+
+    stock_today
+
+
+  end
+
+
+
+
+
+
+  stocks_info = all_stocks.search_()
+
+
+
+  def initialize
+    all_stocks
+
+  end
 
 
   stocks_today
