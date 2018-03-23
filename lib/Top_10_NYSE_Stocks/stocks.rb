1d7a39bc22f0a5acbe9f1470debe192f811e5e8e
Tomas Toledo <trtoledo1977@gmail.com>
2018-03-23 01:46:16 +0000

    CSS selector

--- lib/Top_10_NYSE_Stocks/stocks.rb
+++ lib/Top_10_NYSE_Stocks/stocks.rb
@@ -5,54 +5,45 @@ class Top10NYSEStocks::Stocks
   #return instances of Stock
   #Top10NYSEStocks::StockScraper.new("")
   self.scrape_stocks
-end
+  end
 
-def self.scrape_stocks
-  stocks_today = []
+  def self.scrape_stocks
+    stocks_today = []
 
-  doc = Nokogiri::HTML(open("https://www.nyse.com/index"))
-  all_stocks = doc.search("div.d-container.d-flex-fix.d-vbox.d-nowrap.d-justify-start.ds-simpleTable.d-noscroll").text
+    doc = Nokogiri::HTML(open("https://www.nyse.com/index"))
+    all_stocks = doc.search("div.d-container.d-flex-fix.d-vbox.d-nowrap.d-justify-start.ds-simpleTable.d-noscroll")
+
+#right css selector
+#iterate through each stock listings
+#separate the classes w the same name
+#
+
+
+binding.pry
   #all_stocks.search("ds-row d-hbox d-hover d-pointer")
-  company = all_stocks.colect("ds-row d-hbox d-hover d-pointer").text
-  company.collect do |company|
-      {
-        name: company.css("ds-cell d-flex5").text,
-   		  volume: company.css("ds-cell d-flex2").text,
-   		  last: company.css("ds-cell d-flex2").text,
-        change: company.css("ds-cell d-red d-flex4").text,
-      }
+    company = all_stocks.css("ds-row d-hbox d-hover d-pointer")
+    #company.collect do |company|
+      #  {
+      #    name: company.css("ds-cell d-flex5").text,
+   		#    volume: company.css("ds-cell d-flex2").text,
+   		#    last: company.css("ds-cell d-flex2").text,
+      #    change: company.css("ds-cell d-red d-flex4").text,
+      #    }
 
+    #stock_1 = self.new
+    #stock_1.name = stock.serach("ds-cell d-flex5")
+    #stock_1.volume = "ds-cell d-flex2"
+    #stock_1.last = "ds-cell d-flex2"
+    #stock_1.change = "change 1"
+    #stock_1.url = "url 1"
 
+    stocks_today << self
 
-
-
-
-  def scraping_stocks(all_stocks)
-    stock = all_stocks.colect("ds-row d-hbox d-hover d-pointer").text
-
-
-    stock_1 = self.new
-    stock_1.name = stock.serach("ds-cell d-flex5")
-    stock_1.volume = "ds-cell d-flex2"
-    stock_1.last = "ds-cell d-flex2"
-    stock_1.change = "change 1"
-    stock_1.url = "url 1"
-
-    stock_today << self
-
-    stock_today
+    stocks_today
   end
 
   def initialize
-    all_stocks
 
   end
-
-
-  stocks_today
-
-  end
-
-
 
 end
